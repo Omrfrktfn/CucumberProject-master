cbde17d1417f894bf39cdfad1fb7ff0013c4526f
Fatma Banu Guler <banuguler2016@gmail.com>
2023-09-08 20:49:47 +0200

    Banu

--- src/test/java/team12/pages/US22_US24Pages.java
+++ src/test/java/team12/pages/US22_US24Pages.java
@@ -1,4 +1,128 @@
 package team12.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import team12.utilities.Driver;
+
 public class US22_US24Pages {
+
+    public US22_US24Pages(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//*[@href='/login']")
+    public WebElement logInUs_22;
+
+    @FindBy(xpath = "//*[@placeholder='username']")
+    public WebElement userNameUs_22;
+
+    @FindBy(xpath = "//*[@id='birthDay']")
+    public WebElement dateOfBirthUs_22;
+
+    @FindBy(xpath = "(//h5)[1]")
+    public WebElement addAdminHeaderUs_22;
+
+    @FindBy(xpath = "//*[@id='name']")
+    public WebElement nameUs_22;
+
+    @FindBy(xpath = "//*[text()='Admin Saved']")
+    public WebElement adminSavedAlertUs_22;
+
+    @FindBy(xpath = "(//*[text()='Required'])[1]")
+    public WebElement nameAlertUs_22;
+
+    @FindBy(xpath = "(//*[text()='Required'])[2]")
+    public WebElement surnameAlertUs_22;
+
+    @FindBy(xpath = "(//*[text()='Required'])[3]")
+    public WebElement birthPlaceAlertUs_22;
+
+    @FindBy(xpath = "(//*[text()='Required'])[4]")
+    public WebElement dateOfBirthAlertUs_22;
+
+    @FindBy(xpath = "(//*[text()='Required'])[5]")
+    public WebElement phoneAlertUs_22;
+
+    @FindBy(xpath = "//*[text()='Required']")
+    public WebElement ssnAlertUs_22;
+
+    @FindBy(xpath = "//*[text()='Minimum 11 character (XXX-XX-XXXX)']")
+    public WebElement ssnAlertMinUs_22;
+
+    @FindBy(xpath = "(//*[text()='Required'])[7]")
+    public WebElement userNameAlertUs_22;
+
+    @FindBy(xpath = "//*[text()='Enter your password']")
+    public WebElement passwordAlertUs_22;
+
+    @FindBy(xpath = "(//*[text()='Required'])[4]")
+    public WebElement emailAlertUs_24;
+
+    @FindBy(xpath = "//*[text()='Minimum 11 character (XXX-XX-XXXX)']")
+    public WebElement ssnMinAlertUs_22;
+
+    @FindBy(xpath = "//*[@placeholder='ssn']")
+    public WebElement ssnNumberUs_22;
+
+    @FindBy(xpath = "//*[text()='Please enter valid SSN number']")
+    public WebElement ssnValidAlertUs_22;
+
+    @FindBy(xpath = "(//*[@type='radio'])[1]")
+    public WebElement femaleGenderUs_22;
+
+    @FindBy(xpath = "(//*[@type='radio'])[2]")
+    public WebElement maleGenderUs_22;
+
+//*******************************************
+
+    @FindBy(xpath = "(//button)[1]")
+    public WebElement menuUs_24;
+
+    @FindBy(xpath = "//*[@type='checkbox']")
+    public WebElement isAdviserTeacherCheckbox_24;
+    @FindBy(xpath = "//*[@class='text-white ']")
+    public WebElement hiUs_24;
+
+    @FindBy(xpath = "//*[text()='Team12Admin']")
+    public WebElement Team12AdminUs_24;
+
+    @FindBy(xpath = "(//*[text()='Teacher Management'])[2]")
+    public WebElement teacherManagementUs_24;
+
+    @FindBy(xpath = "(//h5)[1]")
+    public WebElement addTeacherUs_24;
+
+    @FindBy(xpath = "//*[text()='Select lesson']")
+    public WebElement selectLessonUs_24;
+
+    @FindBy(xpath = "//*[text()='Submit']")
+    public WebElement submitUs_24;
+
+    @FindBy(xpath = "//*[text()='Teacher saved successfully']")
+    public WebElement teacherSavedAlertUs_24;
+
+    @FindBy(xpath = "//*[text()='One number']")
+    public WebElement oneNumberNeedAlertUs_24;
+
+    @FindBy(xpath = "//*[text()='One uppercase character']")
+    public WebElement oneUppercaseNeedAlertUs_24;
+
+    @FindBy(xpath = "//*[text()='One lowercase character']")
+    public WebElement oneLowercaseNeedAlertUs_24;
+
+    @FindBy(xpath = "//*[text()='Error:Lesson Programs not found']")
+    public WebElement errorLessonAlertUs_24;
+
+    @FindBy(xpath = "//*[@id='password']")
+    public WebElement passwordUs_24;
+
+    @FindBy(xpath = "(//*[@class='justify-content-start flex-grow-1 fs-5 ps-3 navbar-nav'])[2]")
+    public WebElement mainMenuListUs_24;
+
+    @FindBy(xpath = "//*[@class=' css-19bb58m']")
+    public WebElement selectUs_24;
+
+    @FindBy(xpath = "//*[text()='Basketbol']")
+    public WebElement basketballUs_24;
 }
--- src/test/java/team12/runners/Runner.java
+++ src/test/java/team12/runners/Runner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
                            "rerun:TestOutput/failed_scenario.txt"},
         features = "src/test/resources/features",
         glue = {"team12/stepDefinition", "team12/hooks"},
-        tags = "@aa",
+        tags = "@a5",
         dryRun = false,
         monochrome = true
 
